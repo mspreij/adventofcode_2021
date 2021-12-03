c0d16e5c083a2296ab571631e42a4069fddadc28
mspreij <mspreij@gmail.com>
2021-12-03 12:49:55 +0100

    day 3

---
+++ 3.php
@@ -0,0 +1,41 @@
+<?php
+$values = explode("\n", trim(file_get_contents("3.txt")));
+
+/* 3a: count the bits in 12 slots ($count)
+$count = array_fill(0, 12, 0);
+foreach($values as $value) {
+  foreach(str_split($value) as $i => $bit) {
+    $count[$i] += $bit;
+  }
+}
+$gamma = bindec(join('', array_map(fn($x) => (int) ($x > 500), $count))); // > 500 == "1 = most common"
+$epsilon = $gamma ^ bindec(str_repeat(1, 12)); // just flip bits for "least common"..
+echo $gamma * $epsilon;
+*/
+
+// 3b
+$oxvals = $covals = $values;
+
+for($i=0;$i<12;$i++) {
+  $arrays = [0=>[], 1=>[]];
+  foreach($oxvals as $value) {
+    $bits = str_split($value);
+    $arrays[$bits[$i]][] = $value;
+  }
+  $oxvals = $arrays[(int) (count($arrays[1]) >= count($arrays[0]))];
+  if (count($values) === 1) break;
+}
+$oxygen = bindec(current($oxvals));
+
+for($i=0;$i<12;$i++) {
+  $arrays = [0=>[], 1=>[]];
+  foreach($covals as $value) {
+    $bits = str_split($value);
+    $arrays[$bits[$i]][] = $value;
+  }
+  $covals = $arrays[(count($arrays[0]) > count($arrays[1]))];
+  if (count($covals) === 1) break;
+}
+
+$coscrubs = bindec(current($covals));
+echo $oxygen * $coscrubs;
